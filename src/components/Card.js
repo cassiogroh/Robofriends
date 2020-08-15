e2a1e7727e1902f994de463dd22cf408d15c2654
cassiogroh <cassio_groh@hotmail.com>
2020-08-15 09:50:11 -0300

    Updated readme

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Card = ({name, email, id}) => {
     return (
-        // className carrega styles provenientes do tachyons
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'>
+        // className getting styles from tachyons
+        <div
+        className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-s'
+        >
             <img src={`https://robohash.org/${id}?200x200`} alt='robot'/>
             <div>
                 <h2>{name}</h2>
